099e7a8538167c69633e8ca33adeb8582a6c9e90
wahyuwww <wahyu.aan2508@gmail.com>
2022-04-18 13:06:05 +0700

    ternary

--- Intro-3/Tugas2.js
+++ Intro-3/Tugas2.js
@@ -18,25 +18,20 @@ const name = [
 ];
 
 const seacrh = (keyword, angka, data) => {
-keyword.toLowerCase();
+  keyword.toLowerCase();
   result = name.filter((element) => {
     return element.includes(keyword);
   });
   result.length = angka;
 
   result = result.filter((item) => item !== undefined);
-  
+
   console.log(`Data yang tersedia : ${result.length}`);
 
-  if (result.length) {
-    console.log(result)
-    data(result)
-  } else {
-    console.log("data tidak ada")
-  };
- 
+  result.length ? console.log(result) : console.log("data tidak ada");
+  data(result);
 };
 const seacrhName = (hasil) => {
-  return hasil
-}
-seacrh("ae", 7, seacrhName);
+  return hasil;
+};
+seacrh("ar", 7, seacrhName);
